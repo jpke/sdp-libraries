ff8fb5d3573f3a0e3ca572daeb8bccd1b66a67d0
JP <21163267+jpke@users.noreply.github.com>
2021-05-13 12:05:51 -0400

    add_registry_creds step for anchore

--- libraries/anchore/library_config.groovy
+++ libraries/anchore/library_config.groovy
@@ -4,11 +4,13 @@ fields{
         anchore_engine_url = String	
     }
     optional{
-	image_wait_timeout = int
-	policy_id = String	
-	archive_only = Boolean
-	bail_on_fail = Boolean
-	perform_vulnerability_scan = Boolean
-	perform_policy_evaluation = Boolean
+        image_wait_timeout = int
+        policy_id = String	
+        archive_only = Boolean
+        bail_on_fail = Boolean
+        perform_vulnerability_scan = Boolean
+        perform_policy_evaluation = Boolean
+        docker_registry_credential_id = String
+        docker_registry_name = String
     }
 }
---
+++ libraries/anchore/steps/add_registry_creds.groovy
@@ -0,0 +1,46 @@
+/*
+  Copyright © 2018 Booz Allen Hamilton. All Rights Reserved.
+  This software package is licensed under the Booz Allen Public License. The license can be found in the License file or at http://boozallen.github.io/licenses/bapl
+*/
+
+package libraries.anchore.steps
+
+import groovy.json.*
+
+void call(){
+    stage("Ensure Anchore has Docker Creds"){
+
+        def docker_registry_credential_id = config.docker_registry_credential_id ?: "docker_registry"
+        def docker_registry_name = config.docker_registry_name ?: ""
+
+        def generate_post_data(registry, registry_user, registry_password){
+        return JsonOutput.toJson([
+                registry: "$registry",
+                registry_name: "$registry",
+                registry_type: 'docker_v2',
+                registry_user: "$registry_user",
+                registry_verify: true,
+                registry_pass: "$registry_password"
+            ])
+        }
+
+        node{
+            try {
+                withCredentials([
+                    usernamePassword(credentialsId: config.cred, usernameVariable: 'ANCHORE_USERNAME', passwordVariable: "ANCHORE_PASSWORD"),
+                    usernamePassword(credentialsId: docker_registry_credential_id, usernameVariable: 'REGISTRY_USERNAME', passwordVariable: "REGISTRY_PASSWORD")
+                ]) {
+                    inside_sdp_image "helm", { 
+                        withKubeConfig([credentialsId: "default-kubeconfig" , contextName: "default"]) {
+                            sh "curl --header \"Content-Type: application/json\" -X POST -u $ANCHORE_USERNAME:$ANCHORE_PASSWORD $config.anchore_engine_url/registries -d '${generate_post_data(docker_registry_name, REGISTRY_USERNAME, REGISTRY_PASSWORD)}'"
+
+                            sh 'curl -u $ANCHORE_USERNAME:$ANCHORE_PASSWORD $config.anchore_engine_url/registries'
+                        }
+                    }
+                }
+            } catch (e) {
+                println "anchore add_registry_creds step failed: $e"
+            }
+        }
+    }
+}
