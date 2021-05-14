71ac262703bb495ab55f469006b7424916f7aaa1
JP <21163267+jpke@users.noreply.github.com>
2021-05-14 11:46:32 -0400

    cleanup

--- libraries/anchore/steps/scan_container_image.groovy
+++ libraries/anchore/steps/scan_container_image.groovy
@@ -10,7 +10,6 @@ import groovy.json.*
 void call(){
     stage("Scanning Container Image: Anchore Scan"){
         def images = get_images_to_build()
-        echo "made it here..."
         println images
         def anchore_engine_base_url = config.anchore_engine_url
         def anchore_policy_id = config.policy_id ?: null
