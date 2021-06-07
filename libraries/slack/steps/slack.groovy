11bd2249cd6b2dcc9cd6adcea07c0e2f34e6c3af
JP <21163267+jpke@users.noreply.github.com>
2021-06-07 09:32:45 -0400

    allow for custom slack message

--- libraries/slack/steps/slack.groovy
+++ libraries/slack/steps/slack.groovy
@@ -5,14 +5,17 @@
 
 package libraries.slack.steps
 
-def call(){
+def call(customMessage = ""){
     switch(currentBuild.result){
         case null: // no result set yet means success
         case "SUCCESS":
-          slackSend color: "good", message: "Build Successful: ${env.JOB_URL}"
+          slackSend color: "good", message: customMessage || "Build Successful: ${env.JOB_URL}"
           break;
         case "FAILURE":
-          slackSend color: '#ff0000', message: "Build Failure: ${env.JOB_URL}"
+          slackSend color: '#ff0000', message: customMessage || "Build Failure: ${env.JOB_URL}"
+          break;
+        case "UNSTABLE":
+          slackSend color: '#FFFF00', message: customMessage || "Build Unstable: ${env.JOB_URL}"
           break;
         default:
           echo "Slack Notifier doing nothing: ${currentBuild.result}"
